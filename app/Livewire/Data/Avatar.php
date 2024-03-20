d0145c57eded37f1365d44c182f658c376438bc8
karakhanyans <karakhanyansa@gmail.com>
2024-03-20 13:19:16 +0400

    New Components

---
+++ app/Livewire/Data/Avatar.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Livewire\Data;
+
+use Livewire\Component;
+
+class Avatar extends Component
+{
+    public $src;
+    public $alt;
+
+    public function render()
+    {
+        return view('livewire.data.avatar');
+    }
+}
---
+++ app/Livewire/Navigation/Pagination.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Livewire\Navigation;
+
+use Livewire\Component;
+
+class Pagination extends Component
+{
+    public $pages;
+    public $activePage;
+    
+    public function render()
+    {
+        return view('livewire.navigation.pagination');
+    }
+}
