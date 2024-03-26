73068d06ffee83919f9c0a2c41bc5ef0d99aa6ca
karakhanyans <karakhanyansa@gmail.com>
2024-03-26 11:48:37 +0400

    Hero Block Changes

--- resources/views/livewire/hero.blade.php
+++ resources/views/livewire/hero.blade.php
@@ -1,19 +1,19 @@
 <div class="hero min-h-screen bg-base-100">
     <div class="hero-content flex-col lg:flex-row">
         <div class="basis-2/3">
-            <h1 class="text-5xl sm:text-7xl font-bold">Ship your startup in days, not weeks <br>with <span class="text-secondary">Laravel</span></h1>
-            <p class="mt-6">Laravel Starter Kit is your straightforward Laravel Jetstream, Tailwind,
-                Inertia.js, Vue boilerplate. Streamline your SaaS app development with ready-to-go components for
-                payments, emails, and more. </p>
-            <p class="mt-2 mb-6 tracking-wide">Make development simple and swift with Laravel Starter Kit</p>
-            <button class="btn btn-secondary btn-widest text-lg text-white">
+            <h1 class="text-5xl sm:text-7xl font-bold">This is Demo App made with <span class="text-secondary">Larafast</span></h1>
+            <p class="mt-6">
+                In this demo app you will see the components that Larafast contains, access to admin panel,
+                you can add your own blog posts, register to see the dashboard, etc.
+            </p>
+            <a href="/" class="btn btn-secondary btn-widest text-lg text-white mt-4">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                      stroke="currentColor" class="w-6 h-6">
                     <path stroke-linecap="round" stroke-linejoin="round"
                           d="m3.75 13.5 10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75Z"/>
                 </svg>
-                Get Laravel Starter Kit
-            </button>
+                Get Started
+            </a>
             <livewire:ratings/>
         </div>
         <div class="basis-1/3">
