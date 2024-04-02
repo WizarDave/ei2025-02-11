a4a13564cf112238b524d6c2a5026f834caa7763
karakhanyans <karakhanyansa@gmail.com>
2024-04-02 13:05:17 +0400

    Migrations Fix

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,11 +6,8 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index(Request $request)
+    public function index()
     {
-        $checkout = $request->user()->checkout('pri_01ht4q7xjjbgmeyxr7sgkmzkmt')
-            ->returnTo(route('dashboard'));
-
-        return view('home', ['checkout' => $checkout]);
+        return view('home');
     }
 }
