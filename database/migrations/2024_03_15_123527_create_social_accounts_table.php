10acdcdb88123ba8ddce66421f8a4463bb26611f
karakhanyans <karakhanyansa@gmail.com>
2024-04-17 07:36:45 +0400

    Integer Value for Social Accounts

--- database/migrations/2024_03_15_123527_create_social_accounts_table.php
+++ database/migrations/2024_03_15_123527_create_social_accounts_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('social_accounts', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained('users');
-            $table->bigInteger('account_id');
+            $table->string('account_id');
             $table->string('provider');
             $table->text('token');
             $table->timestamps();
