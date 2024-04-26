63c0be36f8ee9b1972a9a828e6b9c166b128a847
karakhanyans <karakhanyansa@gmail.com>
2024-04-26 10:28:56 +0400

    Modal Updates

--- resources/views/livewire/actions/modal.blade.php
+++ resources/views/livewire/actions/modal.blade.php
@@ -2,7 +2,7 @@
 {{--for example if your modal id is "subscribe" then you can open it using window.subscribe.showModal()--}}
 {{--<button class="btn" onclick="window.subscribe.showModal()">open modal</button>--}}
 
-<dialog id="{{ $id }}" class="modal">
+<dialog id="{{ $id }}" class="modal" wire:ignore.self>
     <div class="modal-box">
         <form method="dialog">
             <button onclick="window.{{ $id }}.closeModal()" class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
