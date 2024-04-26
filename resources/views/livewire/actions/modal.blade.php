59e6e15cecc24fb4debad0e8141ff5b74f9447ec
karakhanyans <karakhanyansa@gmail.com>
2024-04-26 09:49:11 +0400

    Modal Updates

--- resources/views/livewire/actions/modal.blade.php
+++ resources/views/livewire/actions/modal.blade.php
@@ -5,7 +5,7 @@
 <dialog id="{{ $id }}" class="modal">
     <div class="modal-box">
         <form method="dialog">
-            <button wire:click="$emit('closeModal', '{{ $id }}')" class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+            <button onclick="window.{{ $id }}.closeModal()" class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
         </form>
         <h3 class="font-bold text-lg">{{ $title }}</h3>
         <p class="py-4">{{ $description }}</p>
