905fa9138477856d46b19c53497d361121885785
karakhanyans <karakhanyansa@gmail.com>
2024-06-18 21:42:51 +0400

    Jetstream Teams Out of The Box

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,15 +1,15 @@
-<x-home-layout>
+<x-guest-layout>
     <x-authentication-card>
         <x-slot name="logo">
             <x-authentication-card-logo />
         </x-slot>
 
-        <div class="mb-4 text-sm text-gray-600">
+        <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
             {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
         </div>
 
         @session('status')
-            <div class="mb-4 font-medium text-sm text-green-600">
+            <div class="mb-4 font-medium text-sm text-green-600 dark:text-green-400">
                 {{ $value }}
             </div>
         @endsession
@@ -31,4 +31,4 @@
             </div>
         </form>
     </x-authentication-card>
-</x-home-layout>
+</x-guest-layout>
--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -7,8 +7,8 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-12 text-2xl">
-                <p class="text-black text-2xl">Welcome, {{ auth()->user()->name }}</p>
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg p-12 text-2xl">
+                <p class="text-black dark:text-white text-2xl">Welcome, {{ auth()->user()->name }}</p>
             </div>
         </div>
     </div>
