33ef8bedd60c23c0b8156b3842efd2727422c864
karakhanyans <karakhanyansa@gmail.com>
2024-07-25 15:44:44 +0400

    Blog Background

--- resources/views/pages/blog/blog.blade.php
+++ resources/views/pages/blog/blog.blade.php
@@ -1,5 +1,5 @@
 <x-home-layout>
-    <div class="bg-white py-24 sm:py-32">
+    <div class="py-24 sm:py-32">
         <div class="mx-auto max-w-7xl px-6 lg:px-8">
             <div class="mx-auto max-w-2xl lg:mx-0">
                 <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">{{ __('Blog') }}</h2>
