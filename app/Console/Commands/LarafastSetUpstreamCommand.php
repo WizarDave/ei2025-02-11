54119185d75ce716a0c83a31bcce3160706688ab
karakhanyans <karakhanyansa@gmail.com>
2024-07-29 15:46:09 +0400

    Set Upstream Command Updates

--- app/Console/Commands/LarafastSetUpstreamCommand.php
+++ app/Console/Commands/LarafastSetUpstreamCommand.php
@@ -1,43 +1,45 @@
 <?php
 
-    namespace App\Console\Commands;
+namespace App\Console\Commands;
 
-    use Illuminate\Console\Command;
-    use Symfony\Component\Process\Exception\ProcessFailedException;
-    use Symfony\Component\Process\Process;
+use Illuminate\Console\Command;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Process;
 
-    class LarafastSetUpstreamCommand extends Command
+class LarafastSetUpstreamCommand extends Command
+{
+    protected $signature = 'larafast:set-upstream';
+
+    protected $description = 'Set Larafast upstream repository to keep up to date with the latest changes.';
+
+    public function handle(): int
     {
-        protected $signature = 'larafast:set-upstream';
+        // Check if the .git directory exists
+        if (! is_dir(base_path('.git'))) {
+            $this->error('This directory is not a git repository.');
 
-        protected $description = 'Command description';
+            return 1;
+        }
 
-        public function handle()
-        {
-            // Check if the .git directory exists
-            if (!is_dir(base_path('.git'))) {
-                $this->error('This directory is not a git repository.');
+        $commands = [
+            'git remote add larafast https://github.com/karakhanyans-tools/larafast-tall.git',
+        ];
+
+        foreach ($commands as $command) {
+            $process = Process::fromShellCommandline($command);
+
+            try {
+                $process->mustRun();
+                $this->info($process->getOutput());
+            } catch (ProcessFailedException $exception) {
+                $this->error($exception->getMessage());
+
                 return 1;
             }
-
-            $commands = [
-                'git remote add larafast https://github.com/karakhanyans-tools/larafast-tall.git',
-            ];
-
-            foreach ($commands as $command) {
-                $process = Process::fromShellCommandline($command);
-
-                try {
-                    $process->mustRun();
-                    $this->info($process->getOutput());
-                } catch (ProcessFailedException $exception) {
-                    $this->error($exception->getMessage());
-                    return 1;
-                }
-            }
-
-            $this->info('Upstream repository added and fetched successfully.');
-
-            return 0;
         }
+
+        $this->info('Upstream repository added successfully.');
+
+        return 0;
     }
+}
