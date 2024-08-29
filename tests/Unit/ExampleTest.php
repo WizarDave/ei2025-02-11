d7da45a4dbd92bdc27e13f9e3a1d27a0ded96af5
Sergey Karakhanyan <karakhanyansa@gmail.com>
2024-08-29 12:06:19 +0400

    Installed Pest
    - Preparing for Pest 3.0, arch tests

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -1,16 +1,6 @@
 <?php
 
-namespace Tests\Unit;
+test('tests that true is true', static function () {
 
-use PHPUnit\Framework\TestCase;
-
-class ExampleTest extends TestCase
-{
-    /**
-     * A basic test example.
-     */
-    public function test_that_true_is_true(): void
-    {
-        $this->assertTrue(true);
-    }
-}
+    expect(true)->toBeTrue();
+});
