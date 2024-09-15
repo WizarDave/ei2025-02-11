f8abe1ad9ad9897cba9ad2ee824798ef8b84197c
Sergey Karakhanyan <karakhanyansa@gmail.com>
2024-09-15 14:13:59 +0400

    use Filament\Pages\Concerns\ExposesTableToWidgets;

--- app/Filament/Resources/LemonSqueezyOrderResource/Pages/ListOrders.php
+++ app/Filament/Resources/LemonSqueezyOrderResource/Pages/ListOrders.php
@@ -5,9 +5,12 @@ namespace App\Filament\Resources\LemonSqueezyOrderResource\Pages;
 use App\Filament\Resources\LemonSqueezyOrderResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Pages\Concerns\ExposesTableToWidgets;
 
 class ListOrders extends ListRecords
 {
+    use ExposesTableToWidgets;
+
     protected static string $resource = LemonSqueezyOrderResource::class;
 
     protected function getHeaderActions(): array
--- app/Filament/Resources/StripeOrderResource/Pages/ListStripeOrders.php
+++ app/Filament/Resources/StripeOrderResource/Pages/ListStripeOrders.php
@@ -5,9 +5,11 @@ namespace App\Filament\Resources\StripeOrderResource\Pages;
 use App\Filament\Resources\StripeOrderResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Pages\Concerns\ExposesTableToWidgets;
 
 class ListStripeOrders extends ListRecords
 {
+    use ExposesTableToWidgets;
     protected static string $resource = StripeOrderResource::class;
 
     protected function getHeaderActions(): array
